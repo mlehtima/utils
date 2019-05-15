f95a89ac3c227ff6d22d00487522227062e109fe
Juho Hämäläinen <jusa@hilvi.org>
2019-05-15 14:03:21 +0300

    server-sdk.py: Reorganize string handler initialization.

--- server-sdk.py
+++ server-sdk.py
@@ -27,11 +27,11 @@ class WorkerPrinter():
         self.reset()
 
         self._match = []
-        self._match.append((re.compile(r'^.*:\d+:\d+: error:'), ERROR_STR, True))
-        self._match.append((re.compile(r'^.*:\d+:\d+: fatal error:'), ERROR_STR, True))
-        self._match.append((re.compile(r'^.*No rule to make target.*Stop.'), ERROR_STR, True))
-        self._match.append((re.compile(r'^.*:\d+: error:'), ERROR_STR, True))
-        self._match.append((re.compile(r'^.*:\d+:\d+: warning:'), WARN_STR, False))
+        self._match.append((re.compile(r'^.*:\d+:\d+: error:'),                 ERROR_STR,  True    ))
+        self._match.append((re.compile(r'^.*:\d+:\d+: fatal error:'),           ERROR_STR,  True    ))
+        self._match.append((re.compile(r'^.*No rule to make target.*Stop.'),    ERROR_STR,  True    ))
+        self._match.append((re.compile(r'^.*:\d+: error:'),                     ERROR_STR,  True    ))
+        self._match.append((re.compile(r'^.*:\d+:\d+: warning:'),               WARN_STR,   False   ))
 
         self._queue = Queue.Queue()
         self._running = True
